8207249c1cc82bb68ba0911465b335df16979e82
skullcutter02 <=>
2022-02-26 12:11:04 +0800

    Init

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../theme/theme";
+
+// import "../styles/global.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>My Website</title>
+        </Head>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
