d86c3e7435a45dc19912cbde95704b1e6f455b72
skullcutter02 <=>
2022-02-27 09:08:05 +0800

    Added some meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>The Global Institute of Migration</title>
           <link rel="shortcut icon" href={"/logo.png"} />
+          <link rel="canonical" href="http://localhost:3000" /> {/* TODO: change canonical url */}
+          <meta name={"description"} content={"This is the global institute of migration"} />
+          <meta name={"robots"} content={"index, follow"} />
+          <meta property={"og:site_name"} content={"The Global Institute of Migration"} />
         </Head>
         <ChakraProvider theme={theme}>
           <Navbar />
