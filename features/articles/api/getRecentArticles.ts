1767ad76fce0d1368394e5f3dee13d0ce92757a4
skullcutter02 <=>
2022-02-28 10:21:21 +0800

    Changed limit of recent articles from 4 to 3

--- features/articles/api/getRecentArticles.ts
+++ features/articles/api/getRecentArticles.ts
@@ -3,7 +3,7 @@ import directus from "../../../lib/directus";
 const getRecentArticles = async () => {
   const { data } = await directus
     .items("articles")
-    .readByQuery({ sort: ["-created_at"], limit: 4, fields: "*,author.*" });
+    .readByQuery({ sort: ["-created_at"], limit: 3, fields: "*,author.*" });
   return data;
 };
 
