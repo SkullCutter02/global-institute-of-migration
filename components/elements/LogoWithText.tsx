0086ad46578dc11012b874c054755d28ce8e2fae
skullcutter02 <=>
2022-02-28 10:22:34 +0800

    Made navbar logo a link to home

--- components/elements/LogoWithText.tsx
+++ components/elements/LogoWithText.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 import logo from "../../public/logo.png";
 
 const LogoWithText: React.FC = () => {
   return (
     <>
-      <Flex alignItems={"center"} cursor={"pointer"}>
-        <Image src={logo} layout={"fixed"} />
-        <Text fontWeight={"bold"} color={"white"} ml={4} fontSize={23}>
-          WFRM
-        </Text>
-      </Flex>
+      <Link href={"/"}>
+        <Flex alignItems={"center"} cursor={"pointer"}>
+          <Image src={logo} layout={"fixed"} />
+          <Text fontWeight={"bold"} color={"white"} ml={4} fontSize={23}>
+            WFRM
+          </Text>
+        </Flex>
+      </Link>
     </>
   );
 };
