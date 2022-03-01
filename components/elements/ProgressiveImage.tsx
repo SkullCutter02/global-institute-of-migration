ddb12a59c8480f058ae845e6e06b8faa217f4eac
skullcutter02 <=>
2022-03-01 22:23:10 +0800

    Added aspect ratio to progressively loaded image

--- components/elements/ProgressiveImage.tsx
+++ components/elements/ProgressiveImage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Image, ImageProps } from "@chakra-ui/react";
+import { Image, ImageProps, AspectRatio } from "@chakra-ui/react";
 
 import HOST from "../../constants/host";
 import useProgressiveImage from "../../hooks/useProgressiveImage";
 
 interface Props extends ImageProps {
   fromMarkdown?: boolean;
+  aspectRatio?: number;
 }
 
-const ProgressiveImage: React.FC<Props> = ({ src, fromMarkdown = false, ...props }) => {
+const ProgressiveImage: React.FC<Props> = ({ src, fromMarkdown = false, aspectRatio = 16 / 9, ...props }) => {
   const { src: imgSrc, blur: imgBlur } = useProgressiveImage(
     fromMarkdown ? `${src}?width=50&quality=1` : `${HOST}/assets/${src}?width=50&quality=1`,
     fromMarkdown ? src : `${HOST}/assets/${src}`
@@ -16,13 +17,16 @@ const ProgressiveImage: React.FC<Props> = ({ src, fromMarkdown = false, ...props
 
   return (
     <>
-      <Image
-        src={imgSrc}
-        filter={imgBlur ? "blur(20px)" : "none"}
-        transition={imgBlur ? "none" : "filter 0.3s ease-out"}
-        clipPath={"inset(0)"}
-        {...props}
-      />
+      <AspectRatio {...props} ratio={aspectRatio} maxW={props.w}>
+        <Image
+          src={imgSrc}
+          filter={imgBlur ? "blur(20px)" : "none"}
+          transition={imgBlur ? "none" : "filter 0.3s ease-out"}
+          clipPath={"inset(0)"}
+          w={"100%"}
+          h={"100%"}
+        />
+      </AspectRatio>
     </>
   );
 };
